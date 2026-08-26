d358f561e882374b3857c2c15101e3646ad0d011
TimmyTKD <77468344+TimmyTKD@users.noreply.github.com>
2026-08-26 15:41:22 -0400

    Add basic integer combinatorics functions for factorial, nCr, and nPr

---
+++ eulerus/combinatorics.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+namespace eulerus::combinatorics {
+    /* -------------------------------------------------------------------------- */
+    /*                       Integer Combinatorics Functions                       */
+    /* -------------------------------------------------------------------------- */
+
+    // Calculate the factorial of `n` (or `n!`)
+    inline unsigned long long factorial(int n) {
+        if (n < 0) return 0;
+        if (n == 0 || n == 1) return 1;
+
+        // Note: this value will likely overflow for large `n`
+        unsigned long long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    // Calculate the number of `r`-combinations of `n` (or `n` choose `r`)
+    inline unsigned long long nCr(int n, int r) {
+        if (r < 0 || r > n) return 0;
+        if (r == 0 || r == n) return 1;
+        if (r > n / 2) r = n - r;
+        
+        // Note: this value will likely overflow for large `n`
+        unsigned long long result = 1;
+        for (int i = 1; i <= r; i++) {
+            result *= n + 1 - i;
+            result /= i;
+        }
+
+        return result;
+    }
+
+    // Calculate the number of `r`-permutations of `n` (or `n` permute `r`)
+    inline unsigned long long nPr(int n, int r) {
+        if (r < 0 || r > n) return 0;
+        if (r == 0) return 1;
+        if (r == n) return factorial(n);
+        
+        // Note: this value will likely overflow for large `n`
+        unsigned long long result = 1;
+        for (int i = n - r + 1; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+}
