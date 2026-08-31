0929f5191e4c5a082333485f67be6d195b0ae875
TimmyTKD <77468344+TimmyTKD@users.noreply.github.com>
2026-08-31 13:24:53 -0400

    Add generic logarithm, power, and nth root base Function class implementations

--- eulerus/functions.hpp
+++ eulerus/functions.hpp
@@ -156,13 +156,15 @@ namespace eulerus::functions {
     // "Variable" function, which simply returns a function wrapper for the input. Useful for input transformations like f(2x) or polynomials like x^2+x+1
     inline Function var([](auto x) { return x; });
 
+    inline Function pow([](auto x, auto n) { return std::pow(x, n); });
     inline Function sqrt([](auto x) { return std::sqrt(x); });
     inline Function cbrt([](auto x) { return std::cbrt(x); });
+    inline Function nthroot([](auto x, auto n) { return std::pow(x, floating_cast<decltype(x)>(1.0) / n); });
 
     inline Function exp([](auto x) { return std::exp(x); });
     inline Function ln([](auto x) { return std::log(x); }); 
-    inline auto& log = ln;
     inline Function log10([](auto x) { return std::log10(x); });
+    inline Function log([]<typename T, typename T2 = T>(T x, T2 n = std::numbers::e_v<to_floating_t<T>>) { return std::log(x) / std::log(n); });
 
     inline Function sin([](auto x) { return std::sin(x); });
     inline Function cos([](auto x) { return std::cos(x); });
